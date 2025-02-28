b4f6396cdb35ad8f534f5643f8e5ede689bd4c22
Jason Walsh <j@wal.sh>
2025-02-28 10:06:17 -0500

    Consolidate build and publish

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,36 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
         useESM: true,
-      },
-    ],
+        tsconfig: 'tsconfig.json'
+      }
+    ]
   },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
+  testMatch: [
+    '**/__tests__/**/*.test.ts',
+    '**/?(*.)+(spec|test).ts'
+  ],
+  coverageDirectory: './coverage',
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
-    '!src/**/*.test.ts',
+    '!src/__tests__/**'
   ],
   coverageThreshold: {
     global: {
-      branches: 70,
+      branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80,
-    },
-  },
-}
+      statements: 80
+    }
+  }
+};
